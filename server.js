627d0a8cbd0294d8fea155f8df41461b39464f8c
sowmya-9005 <saisowmyasreedonthu@gmail.com>
2025-11-02 22:25:53 +0530

    first commit

--- server.js
+++ server.js
@@ -19,9 +19,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/rights', rightsRoutes);
 app.use("/api/contact", contactRoutes);
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("MongoDB connected"))
-    .catch(err => console.error(err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
